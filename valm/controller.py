107ad21e100df3e87cc967de4d878cecd74c5827
Sam Davey <DaveySam02@gmail.com>
2026-07-31 15:47:53 +0100

    Initial commit

---
+++ valm/controller.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python3
+
+import rclpy
+from rclpy.node import Node
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge
+
+
+class Controller(Node):
+
+    def __init__(self):
+        super().__init__('controller')
+
+        # Camera converter
+        self.bridge = CvBridge()
+
+        # Camera subscriber
+        self.camera_subscription = self.create_subscription(
+            Image,
+            '/color/image_raw',
+            self.image_callback,
+            10
+        )
+
+        self.get_logger().info(
+            'VALM controller started!'
+        )
+
+
+    def image_callback(self, msg):
+    	try:
+        	frame = self.bridge.imgmsg_to_cv2(
+            	msg,
+            	desired_encoding='rgb8'
+        	)
+
+        	self.get_logger().info(
+            	f"Received image: {frame.shape}"
+        	)
+
+    	except Exception as e:
+        	self.get_logger().error(
+            	f"Image conversion failed: {e}"
+        	)
+        
+    
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    node = Controller()
+
+    rclpy.spin(node)
+
+    node.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
